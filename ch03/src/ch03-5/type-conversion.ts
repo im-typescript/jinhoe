f86f892f57fb1efa7663933cbafa2bd022e5f436
jinhoe <jinhoe.kim@imweb.me>
2022-07-23 06:27:50 +0900

    ch03-5 은선님 예시코드 추가

--- ch03/src/ch03-5/type-conversion.ts
+++ ch03/src/ch03-5/type-conversion.ts
@@ -4,7 +4,7 @@
 
 let person: object = {name: "Jack", age: 33};
 console.log((<{name: string}>person).name)
-// console.log(person.name) // 여전히 에러 (위에서만 일시적 변환)
+// console.log(person.name) // 여전히 에러 (위에서만 일시적 변환된 것)
 console.log((person as {age: number}).age)
 
 /* 타입단언 type assertion - 자바스크립트의 타입 변환 구문과 구분하기 위해 새로운 용어
@@ -12,3 +12,9 @@ console.log((person as {age: number}).age)
 * (객체 as 타입)
 */
 
+interface Person{
+    name : string
+    age : number
+}
+let person2 : Person = {name :'Jack', age :33}
+console.log(typeof person2.name)
